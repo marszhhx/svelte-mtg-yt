dae1fb0cd7e718adc9a773ecea0405d820eda880
marszhhx <starzhang0755@gmail.com>
2023-12-21 20:10:34 -0800

    implemented mtn-counter

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+	import Player from './Player.svelte'
+	let redScore = 0;
+	let blueScore = 0;
+	$: blueWon = redScore <= 0;
+	$: redWon = blueScore <= 0;
+	$: gameOver = redWon || blueWon;
+
+	function newGame() {
+		alert('new game!');
+		redScore = 20;
+		blueScore = 20;
+	}
+
+	// function updateBlueScore(e) {
+	// 	const updateScore = e.detail;
+	// 	blueScore += updateScore;
+	// }
+
+</script>
+
+<main>
+	<h1>Magic the Gathering Counter</h1>
+	<div id="controls-container">
+		<Player
+			{gameOver}
+			fontColor="red"
+			won={redWon}
+			winningText="Red wins"
+			bind:score={blueScore}
+		/>
+		<Player 
+			{gameOver}
+			fontColor="blue" 
+			won={blueWon} 
+			winningText="Blue wins" 
+			bind:score={redScore}
+		/>
+	</div>
+	<button on:click={newGame}>Start Game</button>
+</main>
+
+<style>
+	main {
+		width: 80%;
+		padding: 20px;
+		border: solid gray 1px;
+		margin: 0 auto;
+		background-color: wheat;
+		margin: 10vh auto;
+	}
+
+	#controls-container {
+		display: flex;
+	}
+
+	button {
+		display: block;
+		width: 100%;
+		margin-top: 20px;
+		border: solid salmon 1px;
+		background-color: sandybrown;
+		color: rgb(61, 56, 56);
+		font-size: 20px;
+		border-radius: 3px;
+	}
+</style>
---
+++ src/Player.svelte
@@ -0,0 +1,56 @@
+<style>
+    .player {
+        flex-grow: 1;
+    }
+    .plus {
+        background-color: seagreen;
+    }
+    .minus {
+        background-color: brown;
+    }
+    button {
+        font-size: 20px;
+        border-radius: 3px;
+        width: 40px;
+        color: white;
+        font-family: monospace;
+        font-weight: bold;
+    }
+    button[disabled] {
+        color: grey;
+        background-color: lightgrey;
+        cursor: not-allowed;
+    }
+</style>
+
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+    export let score = 0; // letting others know that this is the variable that can be changed
+    export let winningText;
+    export let won;
+    export let fontColor;
+    export let gameOver;
+    function minus() {
+        // dispatch('points', -1);
+        score--;
+    }
+
+    function plus() {
+        score++;
+    }
+
+
+</script>
+
+<div style="color: {fontColor}" class="player">
+    <h2>{score}</h2> 
+    <button 
+        on:click={plus} disabled={gameOver} class="plus">+</button>
+    <button 
+        on:click={minus} disabled={gameOver} class="minus">-</button>
+    {#if won}
+        <h2>{winningText}</h2>
+    {/if}
+
+</div>
